fdda412097cee37dd7cbc7e8b3a091f2b49728c7
manie.coetzee <katGriep0921>
2017-11-12 12:22:40 +0200

    bumbed the version

--- src/lib/src/am-uaa/uaa.service.ts
+++ src/lib/src/am-uaa/uaa.service.ts
@@ -48,7 +48,7 @@ export class UaaService {
     });
   }
 
-  getIdentity(refresh?: boolean, silent?: boolean) : Observable<any> {
+  getIdentity(refresh?: boolean, silent?: boolean) : Observable<any> | any {
     if (refresh){
       this.uaaEventService.broadcast(UaaEvent.LOAD_IDENTITY_START);
       return this.hc.get('/api/uaa/session/identity').map((response)=>{
