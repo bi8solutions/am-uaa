68633dde1d04873771ef25193a0078575920ccca
manie.coetzee <katGriep0921>
2018-04-01 11:06:39 +0200

    upgraded to angular/material 5.2.x - also updated packaging and build scripts

---
+++ src/public_api.ts
@@ -0,0 +1,7 @@
+export { AmUaaModule } from './app/modules/am-uaa/am-uaa.module';
+export { UaaService } from './app/modules/am-uaa/uaa.service';
+export { UaaConfig } from './app/modules/am-uaa/uaa.config';
+export { UaaInterceptor} from './app/modules/am-uaa/uaa.interceptor';
+export { UaaEventService } from './app/modules/am-uaa/uaa.event.service';
+export { IdentityResolver } from './app/modules/am-uaa/identity.resolver';
+export { UaaEvent } from './app/modules/am-uaa/uaa.event';
