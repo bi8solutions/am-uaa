760e199e7e48620fc75421772ec526b1a9b4038c
Francois <francois.kunz@xibsolutions.com>
2018-08-03 10:30:08 +0200

    Added jwt service to public api

--- src/public_api.ts
+++ src/public_api.ts
@@ -4,6 +4,7 @@ export { UaaService } from './app/modules/am-uaa/uaa.service';
 export { UaaConfig } from './app/modules/am-uaa/uaa.config.service';
 export { UaaInterceptor} from './app/modules/am-uaa/uaa.session.interceptor';
 export { UaaJwtInterceptor } from './app/modules/am-uaa/uaa.jwt.interceptor';
+export { JwtService } from './app/modules/am-uaa/jwt.service';
 export { UaaEventService } from './app/modules/am-uaa/uaa.event.service';
 export { IdentityResolver } from './app/modules/am-uaa/identity.resolver';
 export { UaaEvent } from './app/modules/am-uaa/uaa.event';
